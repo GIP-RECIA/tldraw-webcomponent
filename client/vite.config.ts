2fa82780a8d0056d0c1363b397ed36f30a6df20f
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-09-04 10:19:34 +0200

    build(deps): bump
    * @types/node from 18.17.12 to 18.17.14
    
    * prettier from 3.0.2 to 3.0.3
    
    * i18next-http-backend from 2.2.1 to 2.2.2
    
    * react-i18next from 13.2.0 to 13.2.2
    
    * regenerator-runtime from 0.13.11 to 0.14.0

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,21 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/tldraw-webcomponent-[name].[ext]",
-        entryFileNames: "assets/tldraw-webcomponent-[name].js",
-        chunkFileNames: "assets/tldraw-webcomponent-[name].js",
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: process.env.VITE_BASE_URI,
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/tldraw-webcomponent-[name].[ext]',
+          entryFileNames: 'assets/tldraw-webcomponent-[name].js',
+          chunkFileNames: 'assets/tldraw-webcomponent-[name].js',
+        },
       },
     },
-  },
-});
+  });
+};
