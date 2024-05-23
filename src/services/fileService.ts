dccaf90e9972c2a19f91cef9b3924ebe1f9366fe
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-05-23 15:27:29 +0200

    feat!: change post body parameter

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -2,6 +2,6 @@ import { instance as axios } from '../utils/axiosUtils.ts';
 
 const getFile = async (url: string) => await axios.get(url);
 
-const saveFile = async (url: string, blob: string) => await axios.put(url, { blob });
+const saveFile = async (url: string, data: string) => await axios.put(url, { data });
 
 export { getFile, saveFile };
